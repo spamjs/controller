8a39299825d1cc4c015ef8d49f8872ed47acaea0
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-07-27 23:59:06 +0530

    now params passed to new instance as params

--- spamjs.controller.js
+++ spamjs.controller.js
@@ -32,7 +32,7 @@ _define_({
 								var viewModule = module(resp);
 								if(viewModule){
 									viewModule.id=self.id;
-									viewModule.instance().addTo(self.$context);
+									viewModule.instance({ params : self.router.getParams()}).addTo(self.$context);
 								}
 							}
 						} else if(resp && is.Function(resp.addTo)){
@@ -40,7 +40,7 @@ _define_({
 							if(resp.__view_id__){
 								moduleInstance = resp;
 							} else {
-								moduleInstance = res.instance();
+								moduleInstance = res.instance({ params : self.router.getParams()});
 							}
 							moduleInstance.id=self.id;
 							moduleInstance.addTo(self.$context);
